0ba9bd9941bd3af7bb3677773afacedf8bd2020d
Amirerfan Rafati <erfan.rafati@outlook.com>
2021-05-26 18:03:37 +0430

    Added password generation option

--- server.sh
+++ server.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
-apt update
-apt --yes install teeworlds-server 
-teeworlds-server -f autoexe.cfg
+#!/bin/bash
+
+CHARS="qwertyuiopasdfghjklzxcvbnm1234567890"
+
+if [ "$1" = "setup" ]; then
+    apt update
+    apt --yes install teeworlds-server 
+elif [ "$1" = "gen_pass" ]; then
+    for i in {1..6}; do
+        x=$x${CHARS:$RANDOM%${#CHARS}:1}
+    done
+    echo "password is: $x"
+    sed -i "s/\(password\).*/\1 ${x}/" autoexe.cfg
+elif [ "$1" = "run" ]; then
+    teeworlds-server -f autoexe.cfg
+else
+    echo "use \`$0 setup\` to setup the server"
+    echo "use \`$0 gen_pass\` to generate random password"
+    echo "use \`$0 run\` to run the server"
+fi
